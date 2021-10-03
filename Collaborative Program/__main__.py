c470133caf1325408f8ea2375b6593ebbcc8f41d
VicGa <90812185+VicGa@users.noreply.github.com>
2021-10-02 23:30:23 -0300

    import welcome function

--- Collaborative Program/__main__.py
+++ Collaborative Program/__main__.py
@@ -1,11 +1,12 @@
 from Schneider import introduction
-
+from gauna import welcome
 
 
 def main():
     print('Welcome to our collaborative program.')
 
     introduction()
+    welcome()
 
 
 
---
+++ Collaborative Program/gauna.py
@@ -0,0 +1,3 @@
+
+def welcome():
+    print('Hello, my name is Victor D. Gauna')
